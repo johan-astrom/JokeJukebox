d6221827683e6f962bf1a2d46505a5329a2a59e7
johanastrom <johan.astrom89@gmail.com>
2022-06-23 17:20:24 +0200

    Setup project structure

---
+++ JokeJukeboxApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JokeJukeboxApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
