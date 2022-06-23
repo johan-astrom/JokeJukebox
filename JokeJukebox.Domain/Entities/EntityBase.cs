c7145d4a877e8e7dfeb46cdc199a78ae81064ab3
johanastrom <johan.astrom89@gmail.com>
2022-06-23 17:30:03 +0200

    Added entity classes

---
+++ JokeJukebox.Domain/Entities/EntityBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JokeJukebox.Domain.Entities
+{
+    internal class EntityBase
+    {
+    }
+}
