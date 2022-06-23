28de75584c35ee88d0d3c0eeb42a5d1501866391
johanastrom <johan.astrom89@gmail.com>
2022-06-23 17:38:48 +0200

    Added extension method

---
+++ JokeJukebox.Domain/Extensions/DomainServicesExtensions.cs
@@ -0,0 +1,21 @@
+﻿using JokeJukebox.Domain.DataAccess;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JokeJukebox.Domain.Extensions
+{
+    public static class DomainServicesExtensions
+    {
+        public static IServiceCollection AddJokeJukeboxDomain(this IServiceCollection services, IConfiguration config)
+        {
+            return services.AddDbContext<JokeJukeboxContext>(
+                options => options.UseSqlServer(config.GetConnectionString("default")));
+        }
+    }
+}
