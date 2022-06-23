58867755c3e24021bbb6fddaea3a8e1bc6b3a3e1
johanastrom <johan.astrom89@gmail.com>
2022-06-23 17:41:43 +0200

    Referenced extension method

--- JokeJukebox.API/Program.cs
+++ JokeJukebox.API/Program.cs
@@ -1,3 +1,5 @@
+using JokeJukebox.Domain.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Service collection extensions from other assemblies
+builder.Services.AddJokeJukeboxDomain(builder.Configuration);
 
 var app = builder.Build();
 
