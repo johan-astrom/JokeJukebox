d853d5c892605982180ea7e32adea2c5da996f78
johanastrom <johan.astrom89@gmail.com>
2022-06-25 19:33:57 +0200

    Fixed overwrites

--- JokeJukebox.Domain/DataAccess/JokeJukeboxContext.cs
+++ JokeJukebox.Domain/DataAccess/JokeJukeboxContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using JokeJukebox.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,6 +13,8 @@ namespace JokeJukebox.Domain.DataAccess
         public JokeJukeboxContext(DbContextOptions options) : base(options)
         {
         }
-        
+
+        public DbSet<Joke> Jokes { get; set; }
+
     }
 }
