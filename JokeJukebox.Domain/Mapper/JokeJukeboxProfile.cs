842310bc83abe7295ab61c1a1b172436f170ba49
johanastrom <johan.astrom89@gmail.com>
2022-06-25 19:55:58 +0200

    Added automapper

---
+++ JokeJukebox.Domain/Mapper/JokeJukeboxProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using JokeJukebox.Shared.DTO;
+using JokeJukebox.Domain.Entities;
+using JokeJukebox.Shared.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JokeJukebox.Domain.Mapper
+{
+    public class JokeJukeboxProfile : Profile
+    {
+        public JokeJukeboxProfile()
+        {
+            CreateMap<AuthorPostDto, Author>();
+            CreateMap<Author, AuthorGetDto>();
+
+            CreateMap<JokePostDto, Joke>();
+            CreateMap<Joke, JokeGetDto>();
+        }
+    }
+}
